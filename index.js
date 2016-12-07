03ccea9ebf9c3926a721cd7009bd612ce1614776
Kyle VanDeRiet <kylevdr@gmail.com>
2016-12-06 17:11:20 -0700

    Project complete.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var app = express();
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+var messages = [];
+
+app.get('/messages', function(request, response, next) {
+  response.status(200).json({ messages : messages });
+});
+
+app.post('/messages', function (req, res, next) {
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+});
